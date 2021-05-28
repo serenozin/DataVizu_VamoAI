61ae2050b11fefeaba3c95e73a2ce3665ba5f85a
Patryck <patryck@outlook.com>
2021-05-27 21:38:18 -0300

    mortes_rotas update

--- perguntas-projeto-lol.sql
+++ perguntas-projeto-lol.sql
@@ -19,46 +19,46 @@ GROUP BY minuto;
 CREATE VIEW mortes_rotas AS
     SELECT mortes.id_partida, x_pos, y_pos, tempo,
         CASE 
-            WHEN sigla_azul || ' ' || jogador_cacador_azul = matador THEN 'Jungle'
-            WHEN sigla_azul || ' ' || jogador_atirador_azul = matador THEN 'Bottom'
-            WHEN sigla_azul || ' ' || jogador_topo_azul = matador THEN 'Top'
-            WHEN sigla_azul || ' ' || jogador_meio_azul = matador THEN 'Middle'
-            WHEN sigla_azul || ' ' || jogador_suporte_azul = matador THEN 'Support'
+            WHEN sigla_azul || ' ' || jogador_cacador_azul = matador THEN 'Selva'
+            WHEN sigla_azul || ' ' || jogador_atirador_azul = matador THEN 'Inferior'
+            WHEN sigla_azul || ' ' || jogador_topo_azul = matador THEN 'Topo'
+            WHEN sigla_azul || ' ' || jogador_meio_azul = matador THEN 'Meio'
+            WHEN sigla_azul || ' ' || jogador_suporte_azul = matador THEN 'Suporte'
         END AS killer,
         CASE 
-            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_1 THEN 'Jungle'
-            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_1 THEN 'Bottom'
-            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_1 THEN 'Top'
-            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_1 THEN 'Middle'
-            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_1 THEN 'Support'
+            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_1 THEN 'Selva'
+            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_1 THEN 'Inferior'
+            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_1 THEN 'Topo'
+            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_1 THEN 'Meio'
+            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_1 THEN 'Suporte'
         END AS assistant_1,
         CASE 
-            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_2 THEN 'Jungle'
-            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_2 THEN 'Bottom'
-            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_2 THEN 'Top'
-            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_2 THEN 'Middle'
-            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_2 THEN 'Support'
+            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_2 THEN 'Selva'
+            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_2 THEN 'Inferior'
+            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_2 THEN 'Topo'
+            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_2 THEN 'Meio'
+            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_2 THEN 'Suporte'
         END AS assistant_2,
         CASE 
-            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_3 THEN 'Jungle'
-            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_3 THEN 'Bottom'
-            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_3 THEN 'Top'
-            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_3 THEN 'Middle'
-            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_3 THEN 'Support'
+            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_3 THEN 'Selva'
+            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_3 THEN 'Inferior'
+            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_3 THEN 'Topo'
+            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_3 THEN 'Meio'
+            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_3 THEN 'Suporte'
         END AS assistant_3,
         CASE 
-            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_4 THEN 'Jungle'
-            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_4 THEN 'Bottom'
-            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_4 THEN 'Top'
-            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_4 THEN 'Middle'
-            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_4 THEN 'Support'
+            WHEN sigla_azul || ' ' || jogador_cacador_azul = assist_4 THEN 'Selva'
+            WHEN sigla_azul || ' ' || jogador_atirador_azul = assist_4 THEN 'Inferior'
+            WHEN sigla_azul || ' ' || jogador_topo_azul = assist_4 THEN 'Topo'
+            WHEN sigla_azul || ' ' || jogador_meio_azul = assist_4 THEN 'Meio'
+            WHEN sigla_azul || ' ' || jogador_suporte_azul = assist_4 THEN 'Suporte'
         END AS assistant_4,
         CASE 
-            WHEN sigla_vermelho || ' ' || jogador_cacador_vermelho = abatido THEN 'Jungle'
-            WHEN sigla_vermelho || ' ' || jogador_atirador_vermelho = abatido THEN 'Bottom'
-            WHEN sigla_vermelho || ' ' || jogador_topo_vermelho = abatido THEN 'Top'
-            WHEN sigla_vermelho || ' ' || jogador_meio_vermelho = abatido THEN 'Middle'
-            WHEN sigla_vermelho || ' ' || jogador_suporte_vermelho = abatido THEN 'Support'
+            WHEN sigla_vermelho || ' ' || jogador_cacador_vermelho = abatido THEN 'Selva'
+            WHEN sigla_vermelho || ' ' || jogador_atirador_vermelho = abatido THEN 'Inferior'
+            WHEN sigla_vermelho || ' ' || jogador_topo_vermelho = abatido THEN 'Topo'
+            WHEN sigla_vermelho || ' ' || jogador_meio_vermelho = abatido THEN 'Meio'
+            WHEN sigla_vermelho || ' ' || jogador_suporte_vermelho = abatido THEN 'Suporte'
         END AS red_victim
 FROM infos_partida
 INNER JOIN mortes ON infos_partida.id_partida = mortes.id_partida
